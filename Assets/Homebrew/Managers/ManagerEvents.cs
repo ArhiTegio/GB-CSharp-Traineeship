dd9acc9c54848171ee9b613489f6e1be790a6901
Kuznetsov Viktor Vladimirovich <Arhio@yandex.ru>
2020-05-27 14:49:44 +0300

    Тест на стажеровки.

---
+++ Assets/Homebrew/Managers/ManagerEvents.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ManagerEvents",menuName = "Managers/ManagerEvents")]
+public class ManagerEvents : ManagerBase
+{
+
+
+	public GameObject prefab;
+ 
+	public GameObject CreatePrefab(Vector3 pos)
+	{
+		return Instantiate(prefab, pos, Quaternion.identity);
+	}
+
+
+}
