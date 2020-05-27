dbf2e30065ee96fb4669855edd15834a224cb5bf
Kuznetsov Viktor Vladimirovich <Arhio@yandex.ru>
2020-05-27 15:10:22 +0300

    GB C# Стажировка - Тестовое задание. Готово.

---
+++ Assets/[0]Common/ZombiAct.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ZombiAct : MonoBehaviour, ITick
+{
+    private void Awake()
+    {
+        ManagerUpdate.AddTo(this);
+    }
+
+    public void Tick()
+    {
+        Debug.Log("Hello World!");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Toolbox.Get<ManagerEvents>().CreatePrefab(Random.insideUnitSphere * Random.Range(-10, 10));
+        }
+    }
+}
